2c06d2535e88bc2e5bc1105545bc00b971d5030d
ShivaBoya <shivasiddu80@gmail.com>
2025-06-18 10:52:33 +0530

    final

--- DAY5/arrayslevel0.js
+++ DAY5/arrayslevel0.js
@@ -12,3 +12,72 @@ function move(n){
     console.log(n)
 }
 move([0,1,0,3,2,0,0])
+
+function Ones(n){
+    let currCount = 0
+    let maxCount = 0
+    for(let i=0; i<n.length; i++){
+        if(n[i] == 1){
+            currCount++
+        }else{
+            maxCount=Math.max(currCount,maxCount)
+            currCount=0
+        }
+    }
+    return Math.max(currCount,maxCount)
+}
+
+console.log(Ones([1,1,0,0,1,0,1,0,1]))
+
+
+function Missing(num){
+   
+     let n = num.length;
+    let totalSum = (n * (n + 1)) / 2;
+
+    let partialSum = 0;
+    for (let i = 0; i < num.length; i++) {
+        partialSum += num[i];
+    }
+
+    return totalSum - partialSum;
+
+}
+console.log(Missing([1,0,2,4,5]))
+
+class Solution {
+    arraySortedOrNot(arr, n) {
+       for(let i=0; i<arr.length-1; i++){
+         for(let j=i+1; j<arr.length; j++){
+            if(arr[i] > arr[j]){
+                return false
+            }
+            
+         }
+       }
+       return true
+    }
+}
+
+class Solution {
+    countOdd(arr, n) {
+       let oddsum = 0
+       for(let i=0; i<arr.length; i++){
+        if(arr[i] %2 !==0){
+            oddsum ++
+        }
+       }
+       return oddsum
+    }
+}
+
+class Solution {
+    sum(arr,n) {
+     let sum = 0
+     for(let i=0; i<arr.length; i++){
+        sum += arr[i]
+     }
+     return sum
+    }
+   
+}
